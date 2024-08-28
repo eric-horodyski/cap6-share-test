922c6d73c9e3f79cf0430fa8303e30e6770afe6f
Eric Horodyski <horodyski@ionic.io>
2024-08-28 10:26:33 -0400

    Initial commit

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,21 @@
-import './ExploreContainer.css';
+import { IonButton } from "@ionic/react";
+import "./ExploreContainer.css";
+import { Share } from "@capacitor/share";
 
-interface ContainerProps { }
+interface ContainerProps {}
+
+const share = async () => {
+  await Share.share({
+    title: "Check out this Post",
+    text: "Really awesome thing you need to see right now",
+    url: "https://ionicframework.com/docs/native/share",
+  });
+};
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonButton onClick={() => share()}>Share</IonButton>
     </div>
   );
 };
